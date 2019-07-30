3f8b015459b7c10c1c671e59445b75ae9f5c1529
Hannah Matthewson <hannahmatthewson2@gmail.com>
2019-07-30 18:43:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
 
 display_board(board)
